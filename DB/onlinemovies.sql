24c9d1f469abba5d248a30d71c66ee022fd4b4b8
Mr GHOST <shirzad2020@aut.ac.ir>
2022-12-30 14:03:19 +0000

    first commit. movies / actors / account created

---
+++ DB/onlinemovies.sql
@@ -0,0 +1,239 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 30, 2022 at 02:08 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `onlinemovies`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
+
+CREATE TABLE `account` (
+  `username` varchar(40) NOT NULL,
+  `passwords` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `joined_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `membership` bit(1) DEFAULT b'0',
+  `membreship_time` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`username`, `passwords`, `email`, `joined_date`, `membership`, `membreship_time`) VALUES
+('GHOST', '12375845ss', 'allo@gmail.com', '2022-12-30 12:50:43', b'0', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `actor`
+--
+
+CREATE TABLE `actor` (
+  `personal_id` char(10) NOT NULL,
+  `fist_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `age` int(3) GENERATED ALWAYS AS (date_format(from_days(to_days(current_timestamp()) - to_days(`birth_date`)),'%Y')) VIRTUAL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `actor`
+--
+
+INSERT INTO `actor` (`personal_id`, `fist_name`, `last_name`, `birth_date`) VALUES
+('1234567895', 'saeed', 'mozafari', '2013-02-08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `director`
+--
+
+CREATE TABLE `director` (
+  `personal_id` char(10) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `age` int(3) GENERATED ALWAYS AS (date_format(from_days(to_days(current_timestamp()) - to_days(`birth_date`)),'%Y')) VIRTUAL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `director`
+--
+
+INSERT INTO `director` (`personal_id`, `first_name`, `last_name`, `birth_date`) VALUES
+('1234567896', 'Alfred', 'Hichkak', '2000-10-10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genres`
+--
+
+CREATE TABLE `genres` (
+  `genre` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `genres`
+--
+
+INSERT INTO `genres` (`genre`) VALUES
+('Action'),
+('Adventure'),
+('Animation'),
+('Biography'),
+('Comedy'),
+('Crime'),
+('Drama'),
+('Family'),
+('Fantasy'),
+('History'),
+('Horrror'),
+('Music'),
+('Musical'),
+('Mystery'),
+('Romance'),
+('Sci-Fi'),
+('Thriller');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movie`
+--
+
+CREATE TABLE `movie` (
+  `movie_name` varchar(40) NOT NULL,
+  `release_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `movie`
+--
+
+INSERT INTO `movie` (`movie_name`, `release_date`) VALUES
+('lost in the wood', '2016-12-17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movie_actors`
+--
+
+CREATE TABLE `movie_actors` (
+  `movie_name` varchar(40) NOT NULL,
+  `actor_id` char(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movie_genres`
+--
+
+CREATE TABLE `movie_genres` (
+  `movie_name` varchar(40) NOT NULL,
+  `genre` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `movie_genres`
+--
+
+INSERT INTO `movie_genres` (`movie_name`, `genre`) VALUES
+('lost in the wood', 'Action'),
+('lost in the wood', 'Animation'),
+('lost in the wood', 'Horrror');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `actor`
+--
+ALTER TABLE `actor`
+  ADD PRIMARY KEY (`personal_id`);
+
+--
+-- Indexes for table `director`
+--
+ALTER TABLE `director`
+  ADD PRIMARY KEY (`personal_id`);
+
+--
+-- Indexes for table `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`genre`);
+
+--
+-- Indexes for table `movie`
+--
+ALTER TABLE `movie`
+  ADD PRIMARY KEY (`movie_name`);
+
+--
+-- Indexes for table `movie_actors`
+--
+ALTER TABLE `movie_actors`
+  ADD PRIMARY KEY (`movie_name`,`actor_id`),
+  ADD KEY `actor_id` (`actor_id`);
+
+--
+-- Indexes for table `movie_genres`
+--
+ALTER TABLE `movie_genres`
+  ADD PRIMARY KEY (`movie_name`,`genre`),
+  ADD KEY `genre` (`genre`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `movie_actors`
+--
+ALTER TABLE `movie_actors`
+  ADD CONSTRAINT `movie_actors_ibfk_1` FOREIGN KEY (`movie_name`) REFERENCES `movie` (`movie_name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `movie_actors_ibfk_2` FOREIGN KEY (`actor_id`) REFERENCES `actor` (`personal_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `movie_genres`
+--
+ALTER TABLE `movie_genres`
+  ADD CONSTRAINT `movie_genres_ibfk_1` FOREIGN KEY (`movie_name`) REFERENCES `movie` (`movie_name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `movie_genres_ibfk_2` FOREIGN KEY (`genre`) REFERENCES `genres` (`genre`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
