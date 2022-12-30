139f48c2dc75f78bb82de44111d2eb40eece9de8
Mohammad Meymani <mmdspam79@gmail.com>
2022-12-30 18:35:45 +0000

    search version 1.0.1 - only with movie attributes - needs more modifications

--- backend/search.py
+++ backend/search.py
@@ -5,26 +5,41 @@ myCursor = onlineMoviesDB.cursor()
 #search by different movie attributes
 #searching by name of the movie
 def searchByMovieName(movieName):
+    movies = []
     query = 'select * from movie where movie_name = %s'
     myCursor.execute(query,(movieName,))
     for i in myCursor:
-        print(i)
+        movies.append(i)
+    return movies
 #searching by year of the movie
 def searchByMovieYear(publishedYear):
+    movies = []
     query = 'select * from movie where year(release_date) = %s'
     myCursor.execute(query,(publishedYear,))
     for i in myCursor:
-        print(i)
+        movies.append(i)
+    return movies
 #searching by generes
 def searchByMovieGenre(genre):
+    movies = []
     query = 'select * from movie_genres natural join movie where genre = %s'
     myCursor.execute(query,(genre,))
     for i in myCursor:
-        print(i)
+        movies.append(i)
+    return movies
 #test
 print('movie name')
-searchByMovieName('lost in the wood')
+movies = searchByMovieName('lost in the wood')
+for i in movies:
+    print(i)
+print('-----------------------------------------------')
 print('movie year')
-searchByMovieYear('2016')
+movies = searchByMovieYear('2016')
+for i in movies:
+    print(i)
+print('-----------------------------------------------')
 print('movie genre')
-searchByMovieGenre('Action')
+movies = searchByMovieGenre('Action')
+for i in movies:
+    print(i)
+print('-----------------------------------------------')
