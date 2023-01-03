b6259612ba1c0de781c2658683cd876d6b01c77c
Mohammad Meymani <mmdspam79@gmail.com>
2023-01-03 16:25:31 +0000

    search version 1.0.2 - adding administrative search - needs more improvement

--- backend/search.py
+++ backend/search.py
@@ -1,45 +1,272 @@
-import mysql.connector
-#establishing connection
-onlineMoviesDB = mysql.connector.connect(host = 'localhost',username = 'root',password = None,database = 'mySql')
-myCursor = onlineMoviesDB.cursor()
-#search by different movie attributes
-#searching by name of the movie
-def searchByMovieName(movieName):
-    movies = []
-    query = 'select * from movie where movie_name = %s'
-    myCursor.execute(query,(movieName,))
-    for i in myCursor:
-        movies.append(i)
-    return movies
-#searching by year of the movie
-def searchByMovieYear(publishedYear):
-    movies = []
-    query = 'select * from movie where year(release_date) = %s'
-    myCursor.execute(query,(publishedYear,))
-    for i in myCursor:
-        movies.append(i)
-    return movies
-#searching by generes
-def searchByMovieGenre(genre):
-    movies = []
-    query = 'select * from movie_genres natural join movie where genre = %s'
-    myCursor.execute(query,(genre,))
-    for i in myCursor:
-        movies.append(i)
-    return movies
-#test
-print('movie name')
-movies = searchByMovieName('lost in the wood')
-for i in movies:
-    print(i)
-print('-----------------------------------------------')
-print('movie year')
-movies = searchByMovieYear('2016')
-for i in movies:
-    print(i)
-print('-----------------------------------------------')
-print('movie genre')
-movies = searchByMovieGenre('Action')
-for i in movies:
-    print(i)
-print('-----------------------------------------------')
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 01, 2023 at 07:17 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `onlinemovies`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
+drop table account;
+drop table movie;
+drop table director;
+drop table actor;
+drop table genres;
+drop table movie_actors;
+drop table movie_genres;
+drop table movie_directors;
+CREATE TABLE `account` (
+  `username` varchar(40) NOT NULL,
+  `passwords` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `joined_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `membership` bit(1) DEFAULT b'0',
+  `membreship_time` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`username`, `passwords`, `email`, `joined_date`, `membership`, `membreship_time`) VALUES
+('GHOST', '12375845ss', 'allo@gmail.com', '2022-12-30 12:50:43', b'0', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `actor`
+--
+
+CREATE TABLE `actor` (
+  `personal_id` char(10) NOT NULL,
+  `fist_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `age` int(3) GENERATED ALWAYS AS (date_format(from_days(to_days(current_timestamp()) - to_days(`birth_date`)),'%Y')) VIRTUAL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `actor`
+--
+
+INSERT INTO `actor` (`personal_id`, `fist_name`, `last_name`, `birth_date`) VALUES
+('1234567895', 'saeed', 'mozafari', '2013-02-08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `director`
+--
+
+CREATE TABLE `director` (
+  `personal_id` char(10) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `age` int(3) GENERATED ALWAYS AS (date_format(from_days(to_days(current_timestamp()) - to_days(`birth_date`)),'%Y')) VIRTUAL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `director`
+--
+
+INSERT INTO `director` (`personal_id`, `first_name`, `last_name`, `birth_date`) VALUES
+('1234567896', 'Alfred', 'Hichkak', '2000-10-10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genres`
+--
+
+CREATE TABLE `genres` (
+  `genre` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `genres`
+--
+
+INSERT INTO `genres` (`genre`) VALUES
+('Action'),
+('Adventure'),
+('Animation'),
+('Biography'),
+('Comedy'),
+('Crime'),
+('Drama'),
+('Family'),
+('Fantasy'),
+('History'),
+('Horrror'),
+('Music'),
+('Musical'),
+('Mystery'),
+('Romance'),
+('Sci-Fi'),
+('Thriller');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movie`
+--
+
+CREATE TABLE `movie` (
+  `movie_name` varchar(40) NOT NULL,
+  `release_date` date DEFAULT NULL,
+  `url` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `movie`
+--
+
+INSERT INTO `movie` (`movie_name`, `release_date`, `url`) VALUES
+('lost in the wood', '2016-12-17', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movie_actors`
+--
+
+CREATE TABLE `movie_actors` (
+  `movie_name` varchar(40) NOT NULL,
+  `actor_id` char(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movie_directors`
+--
+
+CREATE TABLE `movie_directors` (
+  `movie_name` varchar(40) NOT NULL,
+  `director_id` char(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movie_genres`
+--
+
+CREATE TABLE `movie_genres` (
+  `movie_name` varchar(40) NOT NULL,
+  `genre` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `movie_genres`
+--
+
+INSERT INTO `movie_genres` (`movie_name`, `genre`) VALUES
+('lost in the wood', 'Action'),
+('lost in the wood', 'Animation'),
+('lost in the wood', 'Horrror');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `actor`
+--
+ALTER TABLE `actor`
+  ADD PRIMARY KEY (`personal_id`);
+
+--
+-- Indexes for table `director`
+--
+ALTER TABLE `director`
+  ADD PRIMARY KEY (`personal_id`);
+
+--
+-- Indexes for table `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`genre`);
+
+--
+-- Indexes for table `movie`
+--
+ALTER TABLE `movie`
+  ADD PRIMARY KEY (`movie_name`);
+
+--
+-- Indexes for table `movie_actors`
+--
+ALTER TABLE `movie_actors`
+  ADD PRIMARY KEY (`movie_name`,`actor_id`),
+  ADD KEY `actor_id` (`actor_id`);
+
+--
+-- Indexes for table `movie_directors`
+--
+ALTER TABLE `movie_directors`
+  ADD PRIMARY KEY (`movie_name`,`director_id`),
+  ADD KEY `director_id` (`director_id`);
+
+--
+-- Indexes for table `movie_genres`
+--
+ALTER TABLE `movie_genres`
+  ADD PRIMARY KEY (`movie_name`,`genre`),
+  ADD KEY `genre` (`genre`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `movie_actors`
+--
+ALTER TABLE `movie_actors`
+  ADD CONSTRAINT `movie_actors_ibfk_1` FOREIGN KEY (`movie_name`) REFERENCES `movie` (`movie_name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `movie_actors_ibfk_2` FOREIGN KEY (`actor_id`) REFERENCES `actor` (`personal_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `movie_directors`
+--
+ALTER TABLE `movie_directors`
+  ADD CONSTRAINT `movie_directors_ibfk_1` FOREIGN KEY (`movie_name`) REFERENCES `movie` (`movie_name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `movie_directors_ibfk_2` FOREIGN KEY (`director_id`) REFERENCES `director` (`personal_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `movie_genres`
+--
+ALTER TABLE `movie_genres`
+  ADD CONSTRAINT `movie_genres_ibfk_1` FOREIGN KEY (`movie_name`) REFERENCES `movie` (`movie_name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `movie_genres_ibfk_2` FOREIGN KEY (`genre`) REFERENCES `genres` (`genre`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
