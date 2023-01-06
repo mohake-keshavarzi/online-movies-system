3e3a22856e989370a6565f9873c8e297a582a0da
Mohammad Meymani <mmdspam79@gmail.com>
2023-01-06 15:22:52 +0000

    search version 1.0.3 - add password recovery by having email - needs more improvements

--- backend/search.py
+++ backend/search.py
@@ -42,24 +42,10 @@ def searchUserByEmail(email):
     for i in myCursor:
         account = i
         return account
-#test
-print('movie name')
-movies = searchByMovieName('lost in the wood')
-for i in movies:
-    print(i)
-print('-----------------------------------------------')
-print('movie year')
-movies = searchByMovieYear('2016')
-for i in movies:
-    print(i)
-print('-----------------------------------------------')
-print('movie genre')
-movies = searchByMovieGenre('Action')
-for i in movies:
-    print(i)
-print('-----------------------------------------------')
-print('administrative search:')
-account1 = searchUserByUsername('GHOST')
-print('search by username:',account1)
-account2 = searchUserByEmail('allo@gmail.com')
-print('search by eamil:',account2)
+#search the password by having email address
+def searchPassword(email):
+    query = 'select passwords from account where email = %s'
+    myCursor.execute(query,(email,))
+    for i in myCursor:
+        password = i
+        return password[0]
