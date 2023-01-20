d36235aaefce2351cda1c02824962252dc7906e6
Mohammad Meymani <mmdspam79@gmail.com>
2023-01-20 16:00:25 +0000

    getting score to a movie or edit the score

---
+++ backend/scoring.py
@@ -0,0 +1,19 @@
+import mysql.connector
+import datetime
+#establishing connection
+onlineMoviesDB = mysql.connector.connect(host = 'localhost',username = 'root',password = None,database = 'mySql')
+myCursor = onlineMoviesDB.cursor()
+#function below is used for giving score to a movie or series
+def scoring(username,movieName,score):
+    result = None
+    query1 = 'select * from account_movie_grade where account_username = %s and movie_name = %s'
+    myCursor.execute(query1,(username,movie))
+    for i in myCursor:
+        result = i
+        break
+    if result == None:
+        query2 = 'insert into account_movie_grade (account_username,movie_name,grade,grade_date) values (%s,%s,%s,%s)'
+        myCursor.execute(query2,(username,movieName,score,datetime.date.today()))
+    else:
+        query2 = 'update account_movie_grade set grade = %s where account_username = %s and movie_name = %s'
+        myCursor.execute(query2,(score,))
