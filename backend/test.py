7c88e9eda5e5411f9f9050547304fcd4a684054f
Mohammad Meymani <mmdspam79@gmail.com>
2023-01-20 15:30:12 +0000

    Replace test.py

--- backend/test.py
+++ backend/test.py
@@ -2,6 +2,14 @@ from search import *
 from loginPageFunctions import *
 #testing codes
 #testing search functions
+print('actor id')
+movies = searchByActorID('4759874199')
+for i in movies:
+    print(i)
+print('director id')
+movies = searchByDirectorID('3451221245')
+for i in movies:
+    print(i)
 print('movie name')
 movies = searchByMovieName('lost in the wood')
 for i in movies:
@@ -32,3 +40,5 @@ print('password check')
 print(checkPasswordValidity('1234'),checkPasswordValidity('idgf78f2gfwifgi'))
 print('email check')
 print(checkEmailValidity('mmd@aut.ac.ir'),checkEmailValidity('avdya@ahvcd'))
+print('new account check')
+print(isNewAccount('mmd', 'mmd@gmail.com'),isNewAccount('DreamLord', 'sinohesinohe@gmail.com'),isNewAccount('hasan', 'material.org@yahoo.com'))
